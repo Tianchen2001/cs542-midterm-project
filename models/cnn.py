4d1421491edd590404dce9000bc8d2b8a24a5e3b
Tianchen Wang <tw17@illinois.edu>
2024-02-07 15:51:23 -0600

    initial commit

---
+++ models/cnn.py
@@ -0,0 +1,49 @@
+import torch
+import torch.nn as nn
+
+class CNN(nn.Module):
+    def __init__(self, in_features, num_classes):
+        super(CNN, self).__init__()
+
+        self.model = nn.Sequential(
+            nn.Conv2d(in_features, 16, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+
+            nn.Conv2d(16, 16, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+
+            nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            nn.Flatten(),
+
+            nn.Linear(64 * 4 * 4, 1000),
+            nn.ReLU(),
+
+            nn.Linear(1000, num_classes)
+        )
+    
+    def forward(self, x):
+        out = self.model(x)
+        return out
---
+++ models/lnn.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+
+class LNN(nn.Module):
+    def __init__(self, input_size, num_classes):
+        super(LNN, self).__init__()
+
+        self.model = nn.Sequential(
+            nn.Linear(input_size, 4096),
+            nn.ReLU(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(),
+            nn.Linear(4096, 1000),
+            nn.ReLU(),
+            nn.Linear(1000, num_classes)
+        )
+    
+    def forward(self, x):
+        out = self.model(x)
+        return out
